4a6642d3af9e9262d6ffa4e7ea2e7ba3f515fbbb
BaptisteGrimaldi <grimaldi.baptiste@gmail.com>
2023-09-07 01:23:54 +0200

    checkpositif updateAjustement

--- src/function/checkIfPositive.ts
+++ src/function/checkIfPositive.ts
@@ -2,7 +2,7 @@ export function checkIfPositive(open: string, close: string) {
   let openStock = parseFloat(open);
   let closeStock = parseFloat(close);
 
-  if (closeStock - openStock > 0) {
+  if (closeStock - openStock >= 0) {
     return true;
   } else {
     return false;
