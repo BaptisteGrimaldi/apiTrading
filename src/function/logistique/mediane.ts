a42f60bea6d703b4ffc6e8498a74a293139275b2
Baptiste GRIMALDI <100611587+BaptisteGrimaldi@users.noreply.github.com>
2023-10-01 21:41:04 +0200

    mediane fixe

--- src/function/logistique/mediane.ts
+++ src/function/logistique/mediane.ts
@@ -7,7 +7,7 @@ export function mediane(tableau: number[]) {
     return sortedArray[Math.floor(length / 2)]; // Élément du milieu
   } else {
     // Si la longueur est paire
-    const middle1 = sortedArray[length / 2 - 1];
+    const middle1 = sortedArray[(length / 2) - 1];
     const middle2 = sortedArray[length / 2];
     return (middle1 + middle2) / 2; // Moyenne des deux éléments du milieu
   }
